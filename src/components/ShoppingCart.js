4d0bce493ca0637fbd5096253728a11540eb2f69
Your Name <you@example.com>
2018-01-30 10:58:16 -0500

    working on writing shopping cart

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -3,15 +3,64 @@ import Shipping from './Shipping'
 import Billing from './Billing'
 import Cart from './Cart'
 
+const beers = [
+    {
+      'name': 'Belgian Golden Strong Ale',
+      'brewery': 'Lazy Boy Brewing Company',
+      'abv': 9.3,
+      'img': 'https://i.imgur.com/sDjjBdi.png'
+    },
+    {
+      'name': '3rd Stone Apricot Sour Ale',
+      'brewery': 'Union Craft Brewing Company',
+      'abv': 5.5,
+      'img': 'https://i.imgur.com/S2SXqLn.png'
+    },
+    {
+      'name': 'Almanac Golden Gates Gose',
+      'brewery': 'Almanac Beer Co',
+      'abv': 5,
+      'img': 'https://i.imgur.com/7wC8RUG.png'
+    }
+] 
+
 class ShoppingCart extends Component {
-    render() {
-        return(
-            <div>
-                <Shipping />
-                <Billing />
-                <Cart />
-            </div>
-        )
+    constructor(props) {
+        super(props)
+        this.state = {
+            beers: [
+                {
+                  'name': 'Belgian Golden Strong Ale',
+                  'brewery': 'Lazy Boy Brewing Company',
+                  'abv': 9.3,
+                  'img': 'https://i.imgur.com/sDjjBdi.png'
+                },
+                {
+                  'name': '3rd Stone Apricot Sour Ale',
+                  'brewery': 'Union Craft Brewing Company',
+                  'abv': 5.5,
+                  'img': 'https://i.imgur.com/S2SXqLn.png'
+                },
+                {
+                  'name': 'Almanac Golden Gates Gose',
+                  'brewery': 'Almanac Beer Co',
+                  'abv': 5,
+                  'img': 'https://i.imgur.com/7wC8RUG.png'
+                }
+            ],
+            subtotal: 0,
+            taxPercent: 6,
+            taxTotal: 0,
+            shipping: 0,
+            orderTotal: 0 
+        }
+        render() {
+            return(
+                <div>
+                    
+                </div>
+            )
+        }
     }
 }
 
