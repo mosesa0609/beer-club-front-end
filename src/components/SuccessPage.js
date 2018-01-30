5af185fae4bc16946fa1497ce51de946903d6ab2
Francis Julian Addison <faddison@Franciss-MacBook.local>
2018-01-30 14:10:43 -0500

    component is somewhat styled

--- src/components/SuccessPage.js
+++ src/components/SuccessPage.js
@@ -1,19 +1,23 @@
 import React, {Component} from 'react'
+import { Card, Col } from 'react-materialize'
+import './SuccessPage.css'
 
 class SuccessPage extends Component {
   render () {
     return (
-      <div className='receipt-container'>
-        <h3>Purchase Summary</h3>
-        <div className='individual-item'>
-          <p>Item</p>
-          <p>Quantity</p>
-          <p>Shipping Method</p>
-          <p>Shipping cost</p>
-          <p>Total</p>
-        </div>
-        <div className='total-order'>
-          <p>Order Total</p>
+      <div className='component-container'>
+        <div className='receipt-container'>
+          <h3>Order Summary</h3>
+          <div className='individual-item'>
+            <p>Item</p>
+            <p>Quantity</p>
+            <p>Shipping Method</p>
+            <p>Shipping cost</p>
+            <p>Total</p>
+          </div>
+          <div className='total-order'>
+            <p>Order Total</p>
+          </div>
         </div>
       </div>
     )
