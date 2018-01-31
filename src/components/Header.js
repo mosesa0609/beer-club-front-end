9dc219c59df57b896abe33d9fca7ecc49fb0c30d
Alonzo Moses <alonzo.k.moses@gmail.com>
2018-01-31 18:42:05 -0500

    adding search field functionality - Sandro and Alonzo pair program

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,36 @@ import {BrowserRouter as Router, Route, Link, Redirect, Switch} from 'react-rout
 import './Header.css'
 import Landing from './Landing'
 import Shop from './Shop'
+// import InitialSearch from '../src/components/InitialSearch'
 
 // import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
 
 class Header extends Component {
+  // constructor (props) {
+  //   super(props)
+  //   this.state = {
+  //     firstVisit: false
+  //   }
+  // }
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      search: '',
+      searchedResults: {}
+    }
+  }
+
+  handleSearch (e) {
+    this.setState({
+      search: e.target.value
+    })
+  }
+
+  searchedContent (e) {
+    e.preventDefault()
+  }
+
   render () {
     return (
       <div>
@@ -15,21 +41,29 @@ class Header extends Component {
           <div>
             <div>
               <Navbar className='page-header' brand='Dollar Beer Club' right>
-              {/* <div className='waves-effect waves-light btn #ff8f00 amber darken-3 centered-header'><Link to='/shop'>Get A Pint Today!</Link></div> */}
-              <NavItem href='#'><div className=''><Link to='/'>Sign In</Link></div></NavItem>
-              <NavItem href='#'><div className=''><Link to='/register'>Sign Up</Link></div></NavItem>
-              <NavItem href='#'><div className=''>About</div></NavItem>
-              <NavItem href='#'><div className=''><Link to='/shop'>Crafted Beers</Link></div></NavItem>
-            </Navbar>
+                <div class='nav-wrapper'>
+                  <form>
+                    <div class='input-field'>
+                      <input id='search' type='search' onChange={(e) => this.handleSearch(e)} required />
+                      <label class='label-icon' for='search'><i class='material-icons'>search</i></label>
+                      <i class='material-icons'>close</i>
+                    </div>
+                  </form>
+                </div>
+                <NavItem href='#'><div className=''><Link to='/'>Sign In</Link></div></NavItem>
+                <NavItem href='#'><div className=''><Link to='/register'>Sign Up</Link></div></NavItem>
+                <NavItem href='#'><div className=''>About</div></NavItem>
+                <NavItem href='#'><div className=''><Link to='/shop'>Crafted Beers</Link></div></NavItem>
+              </Navbar>
             </div>
 
             <div>
               <Switch>
-              <Route path='/shop' component={Shop} />
-              <Route path='/logon' component={Shop} />
-              <Route path='/register' component={Shop} />
-              <Route path='/' component={Landing} />
-            </Switch>
+                <Route path='/shop' component={Shop} />
+                <Route path='/logon' component={Shop} />
+                <Route path='/register' component={Shop} />
+                <Route path='/' component={Landing} />
+              </Switch>
             </div>
           </div>
         </Router>
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -3,47 +3,46 @@ import { Card, CardTitle, Button } from 'react-materialize'
 import axios from 'axios'
 import { Parallax } from 'react-parallax'
 // import requestDBC from '../../src/request'
+import { queryBrewery } from './Utils'
 
 class Shop extends Component {
   constructor (props) {
     super(props)
     this.state = {
       dummyPic: 'https://stamandtrade.com/wp-content/uploads/2017/03/no-image-available.jpg',
-      searchResults: [
-        // {
-        //   name: 'Belgian Golden Strong Ale',
-        //   price: 7,
-        //   brewery: 'Lazy Boy Brewing Company',
-        //   abv: 9.3,
-        //   style: 'something here',
-        //   img: 'https://i.imgur.com/sDjjBdi.png'
-        // },
-        // {
-        //   name: '3rd Stone Apricot Sour Ale',
-        //   price: 7,
-        //   brewery: 'Union Craft Brewing Company',
-        //   abv: 5.5,
-        //   style: 'something here',
-        //   img: 'https://i.imgur.com/S2SXqLn.png'
-        // }
-      ]
+      query: '',
+      hasSearched: false,
+      searchResults: [],
     }
   }
 
+  handleSearchInput = (searchText) => {
+    console.log(searchText)
+    this.setState({query: searchText})
+  }
+
+  onSubmitQuery = (searchText) => {
+    queryBrewery(searchText).then(beers => {
+      this.setState(prevState => ({
+        searchResults: beers
+      }))
+    })
+  }
+
   componentDidMount () {
-    axios
-      .get(`http://localhost:4000/search/bud`)
-      .then(res => {
-        const beers = res.data.map(data => data)
-        this.setState(prevState => ({
-          searchResults: beers
-        }))
-      })
+    // axios
+    //   .get(`http://localhost:4000/search/heineken`)
+    //   .then(res => {
+    //     const beers = res.data.map(data => data)
+    //     this.setState(prevState => ({
+    //       searchResults: beers
+    //     }))
+    //   })
   }
 
   render () {
     let beerKeg = this.state.searchResults.map((beer, index) => {
-      console.log(beer.labels)
+      console.log(beer)
       return (
         <Card className='card hoverable col s12 m3' header={<CardTitle reveal image={beer.labels ? beer.labels.large : this.state.dummyPic} waves='light' />} title={<h5 className='' style={{fontSize: 20}}> {beer.name}</h5>}
           reveal={[
