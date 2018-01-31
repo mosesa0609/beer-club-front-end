127cea7bb1a1f5d357ca1f07e97e71267f7b2aae
Francis Julian Addison <faddison@Franciss-MacBook.local>
2018-01-31 12:45:08 -0500

    working

--- src/App.js
+++ src/App.js
@@ -2,13 +2,7 @@ import React, { Component } from 'react'
 // import Header from '../src/components/Header'
 // import Footers from '../src/components/Footer'
 import Landing from '../src/components/Landing'
-<<<<<<< HEAD
-import ShoppingCart from '../src/components/ShoppingCart'
-import Billing from './components/Billing'
-
-=======
 // import Shop from '../src/components/Shop'
->>>>>>> shop-page-alonzo
 // import './App.css';
 
 class App extends Component {
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import {Navbar, NavItem} from 'react-materialize'
 import {BrowserRouter as Router, Route, Link, Redirect, Switch} from 'react-router-dom'
-import './Header.css'
+import '../styles/Header.css'
 import Landing from './Landing'
 import Shop from './Shop'
 
@@ -12,26 +12,26 @@ class Header extends Component {
     return (
       <div>
         <Router>
-        <div>
           <div>
-            <Navbar className='#ffca28 amber lighten-1' brand='Dollar Beer Club' right>
+            <div>
+              <Navbar className='#ffca28 amber lighten-1' brand='Dollar Beer Club' right>
               {/* <div className='waves-effect waves-light btn #ff8f00 amber darken-3 centered-header'><Link to='/shop'>Get A Pint Today!</Link></div> */}
               <NavItem href='#'><div className=''><Link to='/'>Sign In</Link></div></NavItem>
               <NavItem href='#'><div className=''><Link to='/register'>Sign Up</Link></div></NavItem>
               <NavItem href='#about'><div className=''>About</div></NavItem>
               <NavItem href='#products'><div className=''><Link to='/shop'>Crafted Beers</Link></div></NavItem>
             </Navbar>
-          </div>
+            </div>
 
-          <div>
-            <Switch>
+            <div>
+              <Switch>
               <Route path='/shop' component={Shop} />
-              <Route path='/logon' component={Shop}/>
-              <Route path='/register' component={Shop}/>
+              <Route path='/logon' component={Shop} />
+              <Route path='/register' component={Shop} />
               <Route path='/' component={Landing} />
             </Switch>
+            </div>
           </div>
-        </div>
         </Router>
       </div>
     )
