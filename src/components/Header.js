6012e588ce7c194bba058e9880bec352ecfa2ca5
Marquet Reid <you@example.com>
2018-02-01 16:17:17 -0500

    working on merging

--- src/components/Header.js
+++ src/components/Header.js
@@ -40,7 +40,6 @@ class Header extends Component {
       <div>
         <Router>
           <div>
-<<<<<<< HEAD
             <div>
               <Navbar className='#ffca28 amber lighten-1' brand='Dollar Beer Club' right>
                 {/* <div className='waves-effect waves-light btn #ff8f00 amber darken-3 centered-header'><Link to='/shop'>Get A Pint Today!</Link></div> */}
@@ -56,28 +55,10 @@ class Header extends Component {
                 <Route path='/shop' component={Shop} />
                 <Route path='/logon' component={Login}/>
                 <Route path='/register' component={Shop}/>
+                <Route path='/about' component={About}/>
                 <Route path='/' component={Landing} />
               </Switch>
             </div>
-=======
-            <Navbar className='#ffca28 amber lighten-1' brand='Dollar Beer Club' right>
-              {/* <div className='waves-effect waves-light btn #ff8f00 amber darken-3 centered-header'><Link to='/shop'>Get A Pint Today!</Link></div> */}
-              <NavItem href='#'><div className=''><Link to='/'>Sign In</Link></div></NavItem>
-              <NavItem href='#'><div className=''><Link to='/register'>Sign Up</Link></div></NavItem>
-              <NavItem href='#about'><div className=''><Link to='/about'>About</Link></div></NavItem>
-              <NavItem href='#products'><div className=''><Link to='/shop'>Crafted Beers</Link></div></NavItem>
-            </Navbar>
-          </div>
-
-          <div>
-            <Switch>
-              <Route path='/shop' component={Shop} />
-              <Route path='/logon' component={Shop}/>
-              <Route path='/register' component={Shop}/>
-              <Route path='/about' component={About}/>
-              <Route path='/' component={Landing} />
-            </Switch>
->>>>>>> about-component
           </div>
         </Router>
       </div>
