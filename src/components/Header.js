8b2759a9b5159bf2bac04dcc328232c3384857e7
Marquet Reid <you@example.com>
2018-02-01 11:01:42 -0500

    fixed display bug

--- src/components/Header.js
+++ src/components/Header.js
@@ -39,22 +39,24 @@ class Header extends Component {
       <div>
         <Router>
           <div>
-            <Navbar className='#ffca28 amber lighten-1' brand='Dollar Beer Club' right>
-              {/* <div className='waves-effect waves-light btn #ff8f00 amber darken-3 centered-header'><Link to='/shop'>Get A Pint Today!</Link></div> */}
-              <NavItem href='#'><div className=''><Link to='/logon'>Sign In</Link></div></NavItem>
-              <NavItem href='#'><div className=''><Link to='/register'>Sign Up</Link></div></NavItem>
-              <NavItem href='#about'><div className=''>About</div></NavItem>
-              <NavItem href='#products'><div className=''><Link to='/shop'>Crafted Beers</Link></div></NavItem>
-            </Navbar>
-          </div>
+            <div>
+              <Navbar className='#ffca28 amber lighten-1' brand='Dollar Beer Club' right>
+                {/* <div className='waves-effect waves-light btn #ff8f00 amber darken-3 centered-header'><Link to='/shop'>Get A Pint Today!</Link></div> */}
+                <NavItem href='#'><div className=''><Link to='/logon'>Sign In</Link></div></NavItem>
+                <NavItem href='#'><div className=''><Link to='/register'>Sign Up</Link></div></NavItem>
+                <NavItem href='#about'><div className=''>About</div></NavItem>
+                <NavItem href='#products'><div className=''><Link to='/shop'>Crafted Beers</Link></div></NavItem>
+              </Navbar>
+            </div>
 
-          <div>
-            <Switch>
-              <Route path='/shop' component={Shop} />
-              <Route path='/logon' component={Login}/>
-              <Route path='/register' component={Shop}/>
-              <Route path='/' component={Landing} />
-            </Switch>
+            <div>
+              <Switch>
+                <Route path='/shop' component={Shop} />
+                <Route path='/logon' component={Login}/>
+                <Route path='/register' component={Shop}/>
+                <Route path='/' component={Landing} />
+              </Switch>
+            </div>
           </div>
         </Router>
       </div>
