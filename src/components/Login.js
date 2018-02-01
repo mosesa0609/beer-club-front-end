4bfc2da7579a3654a72f7a8e2b9b424b0e0d8b57
Marquet Reid <you@example.com>
2018-02-01 10:12:47 -0500

    added new styling and logic for handling input

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,17 +4,36 @@ import '../styles/Login.css'
 import Billing from './Billing'
 
 class Login extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            email: '',
+            password: ''
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({
+            ...this.state,
+            [e.target.name]: e.target.value
+        })
+    }
+
     render () {
         return (
-            <div className="login-container">
-            <Card className='#ffca28 amber lighten-5'>
-                <h3>Login Page</h3>
-                <form>
-                <Input className='#ffca28'type="text" name="email" placeholder="Email" />
-                <Input type="password" name="password" placeholder="Password" />
-                <Button className='#ff8f00 amber darken-3' type="submit">Login</Button>
-                </form>
-            </Card>
+            <div className='bgImg'>
+                <div className='container'>
+                    <div className='border' style={{padding: 10}}>
+                        <div className='container' style={{width: 450, height: 500}}>
+                            <Row className='#f5f5f5 grey lighten-4' style={{padding: 30}}>
+                                <Input onChange = {this.handleChange} className='#ffca28' type="text" name="email" placeholder="Email" s={12} />
+                                <Input onChange = {this.handleChange} type="password" name="password" placeholder="Password" s={12} />
+                                <Button waves='light' className='login-button right' type="submit">Login</Button>
+                            </Row>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
