6e4785457fdbf26c1ba1bb4d8aec70bd4723abdb
Marquet Reid <you@example.com>
2018-02-04 12:01:26 -0500

    Alonzo fixed shop component

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,65 +1,63 @@
 import React, {Component} from 'react'
-import { Card, CardTitle, Button, Container } from 'react-materialize'
+import { Card, CardTitle, Button } from 'react-materialize'
 import axios from 'axios'
 import { Parallax } from 'react-parallax'
 import requestDBC from '../../src/request'
 import { queryBrewery } from './Utils'
 import Results from './Results';
 import Search from './Search';
+import Container from './Container'
 
 class Shop extends Component {
-  // render () {
-  //   return (
-  //     <div>
-  //       <div>
-  //         <Container />
-  //         <div>
-  //           <div style={{backgroundRepeat: 'no-repeat',
-  //                       backgroundSize: 'cover',
-  //                       display: 'flex',
-  //                       justifyContent: 'center',
-  //                       alignItems: 'center',
-  //                       height: '40vh'}}>
+  constructor (props) {
+    super(props)
+    this.state = {
+      dummyPic: 'https://stamandtrade.com/wp-content/uploads/2017/03/no-image-available.jpg',
+      query: '',
+      hasSearched: false,
+      searchResults: [],
+    }
+  }
 
-  //           </div>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   )
-  // }
-  render () {
-    let beerKeg = this.state.searchResults.map((beer, index) => {
-      console.log(beer)
-      return (
-        <Card className='card hoverable col s12 m3' header={<CardTitle reveal image={beer.labels ? beer.labels.large : this.state.dummyPic} waves='light' />} title={<h5 className='' style={{fontSize: 20}}> {beer.name}</h5>}
-          reveal={[
-            <div>
-              <h4>$7</h4>
-              <h5>Description: {beer.description}</h5>
-              <h5>Brew Style: {beer.style.name}</h5>
-              <h5>ABV: {beer.abv}</h5>
-            </div>
-          ]}>
-          <Button floating className='red hoverable halfway-fab' waves='light' icon='add' style={{position: 'absolute', bottom: 90, right: 8}} />
-        </Card>
-      )
+  handleSearchInput = (searchText) => {
+    console.log(searchText)
+    this.setState({query: searchText})
+  }
+
+  onSubmitQuery = (searchText) => {
+    queryBrewery(searchText).then(beers => {
+      this.setState(prevState => ({
+        searchResults: beers,
+        hasSearched: true
+      }))
     })
+  }
+
+  onSearchAgain = () => {
+    this.setState({
+      hasSearched: false,
+      query: ''
+    })
+  }
+  render () {
     return (
-      
-      <div className='row'>
-      <div className='nav-wrapper'>
-                  <form>
-                    <div className='input-field black centered-header'>
-                      <input id='search' type='search' onChange={(e) => this.handleSearch(e)} required />
-                      <label className='label-icon' for='search'><i class='material-icons'>search</i></label>
-                      <i className='material-icons'>close</i>
-                    </div>
-                  </form>
-                </div>
-        {beerKeg}
+      <div>
+        <div>
+          <Container />
+          <div>
+            <div style={{backgroundRepeat: 'no-repeat',
+                        backgroundSize: 'cover',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        height: '40vh'}}>
+
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
+  
 }
-
 export default Shop
